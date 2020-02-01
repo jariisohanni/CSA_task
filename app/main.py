faadaf118776e81a9a46399b436df15223ef6640
Jari Isohanni <jari.isohanni@gmail.com>
2020-02-01 16:28:12 +0200

    Added some code to main.py which act's as entry for terminal client

--- app/main.py
+++ app/main.py
@@ -1,2 +1,13 @@
 def testOK():
     return True
+
+def main():
+    print("*************")
+    print("WELCOME TO NÄSÄ AGENCY")
+    print("*************")
+    print(" ")
+    print("Please enter your agent id:")
+    
+
+
+main()
