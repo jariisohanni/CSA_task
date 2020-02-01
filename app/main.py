7bf234c44a35fead608b7ff2aa7b6cbf46cd0866
Jari Isohanni <jari.isohanni@gmail.com>
2020-02-01 16:41:11 +0200

    Added some code to main.py which act's as entry for terminal client

--- app/main.py
+++ app/main.py
@@ -2,12 +2,24 @@ def testOK():
     return True
 
 def main():
-    print("*************")
-    print("WELCOME TO NÄSÄ AGENCY")
-    print("*************")
+    print("**************************")
+    print("WELCOME TO NÄSA AGENCY")
+    print("**************************")
     print(" ")
-    print("Please enter your agent id:")
-    
+
+    agent_id = ""
+    logged_in = False
+    while logged_in == False and agent_id != "exit":
+
+        while agent_id == "":
+            print("Please enter your agent id (or type exit):")
+            agent_id = input()
+
+        ##VERIFY USER ID FROM USERS DATA BASE
+
+        ##ASK FOR PASSWORD
+
+        ##IF OK LIST USER FILES
 
 
 main()
