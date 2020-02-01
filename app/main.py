166e7dc82af6b98d03bda32b2bf9c2def7305f81
Jari Isohanni <jari.isohanni@gmail.com>
2020-02-01 21:40:45 +0200

    First attempt to create login to ssystem

--- app/main.py
+++ app/main.py
@@ -1,3 +1,7 @@
+
+from app.users.user_manager import  UserManager
+
+
 def testOK():
     return True
 
@@ -7,13 +11,10 @@ def main():
     print("**************************")
     print(" ")
 
-    agent_id = ""
-    logged_in = False
-    while logged_in == False and agent_id != "exit":
+    while loginPhase():
+        pass
 
-        while agent_id == "":
-            print("Please enter your agent id (or type exit):")
-            agent_id = input()
+
 
         ##VERIFY USER ID FROM USERS DATA BASE
 
@@ -21,5 +22,21 @@ def main():
 
         ##IF OK LIST USER FILES
 
+def loginPhase():
+    agent_id = ""
+    logged_in = False
 
+    while logged_in == False and agent_id != "exit":
+
+        while agent_id == "":
+            print("Please enter your agent id (or type exit):")
+            agent_id = input()
+            if user_manager.find_user_with_id(agent_id) is not None:
+                print("Found user, logging in")
+            else:
+                agent_id = ""
+
+
+
+user_manager = UserManager()
 main()
---
+++ app/users/user_manager.py
@@ -0,0 +1,6 @@
+class UserManager():
+
+    user_list = []
+
+    def find_user_with_id(self,id):
+        return None
