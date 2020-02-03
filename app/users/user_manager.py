d6548269960a5b0c4b0fbd2b603f971522260429
Jari Isohanni <jari.isohanni@gmail.com>
2020-02-03 21:15:20 +0200

    added skeleton for user and user_manager + tests

--- app/users/user_manager.py
+++ app/users/user_manager.py
@@ -1,6 +1,14 @@
-class UserManager():
+class UserManager:
+    def __init__(self):
+        self.user_list = []
 
-    user_list = []
 
-    def find_user_with_id(self,id):
+    def addUser(self,user):
+        self.user_list.append(user)
+
+    def find_user_with_id(self,toFind):
+        for user in self.user_list:
+            if user.username == toFind:
+                return user
+
         return None
--- tests/test_main.py
+++ tests/test_main.py
@@ -4,18 +4,27 @@ import random
 
 from app import main
 from app.encryption import encryption_engine
+from app.users.user_manager import UserManager
+from app.users.user import User
+
 
 def test_setup():
-    assert( main.testOK() == True)
+    assert (main.testOK() == True)
+
+
+def test_user_manager():
+    um = UserManager()
+    um.addUser(User("jari", "testi"))
+    assert (um.find_user_with_id("jari") is not None)
+    assert (um.find_user_with_id("joni") is not None)
 
 def test_hash():
-
     test_count = 1
     max_test = 100
     match_found = False
     while test_count <= max_test:
         p1 = randomString(10)
-        p2 = randomString(10)
+        p2 = randomString(11)
         sha1 = encryption_engine.hash_text(p1)
         sha2 = encryption_engine.hash_text(p2)
         if sha1 == sha2:
@@ -23,12 +32,10 @@ def test_hash():
 
         test_count += 1
 
-
-    assert(match_found == False )
+    assert (False == match_found)
 
 
 def randomString(stringLength=10):
     """Generate a random string of fixed length """
     letters = string.ascii_lowercase
     return ''.join(random.choice(letters) for i in range(stringLength))
-
