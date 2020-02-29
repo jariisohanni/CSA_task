17973285f8a6a00a85141449a1be785b4176a793
Tytti Lyden-Pohjonen <TyttiLyden@ahaggblom.fi>
2020-02-29 14:14:00 +0200

    Changes made: - user management with csv files - password masking

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from users.user_manager import UserManager
+from users.user import User, Role
 from msvcrt import getch
 import getpass, sys
 
@@ -31,16 +32,36 @@ def main():
 def loginPhase():
     agent_id = ""
     logged_in = False
+    pwd = ""
 
     while logged_in == False and agent_id != "exit":
-
-        while agent_id == "":
-            print("Please enter your agent id and password (or type exit:)")
+            manager = UserManager()
+            print("Please enter your agent id:")
             agent_id = input()
             pwd = mask_input()
-            if manager.find_user_with_id(agent_id, pwd) is not None:
+            role = manager.find_user_with_id(agent_id, pwd)
+            if role is not None:
                 print("Access granted")
-                
+                if role == 1:
+                    ans = True
+                    while ans:
+                        print("""
+                        Please select function
+                        1.Manage users
+                        2.Access filesystem
+                        4.Exit/Quit
+                        """)
+                        ans = input()
+                        if ans=="1":
+                            manageUsers()
+                        elif ans=="2":
+                          print("Something else")
+                          logged_in == True
+                        elif ans=="4":
+                          print("Goodbye")
+                          return None
+                else:
+                    print("Not admin")
             else:
                 agent_id = ""
 
@@ -49,10 +70,8 @@ def mask_input(prompt='Password: '):
         Prompt for a password and masks the input.
         Returns the hashed string from the password entered by the user.
     '''
-
     if sys.stdin is not sys.__stdin__:
-        pwd = getpass.getpass(prompt)
-        return pwd
+        return getpass.getpass(prompt)
     else:
         pwd = ""        
         sys.stdout.write(prompt)
@@ -75,8 +94,38 @@ def mask_input(prompt='Password: '):
                 sys.stdout.write('*')
                 sys.stdout.flush()                
                 pwd = pwd + char
+        return pwd    
+        
+def manageUsers():
+    manager = UserManager()
+    
+    ans = True
+    while ans:
+        print("""
+                        Please select function
+                        1.Add user
+                        2.Delete user
+                        3.Return to main menu
+                        """)
+        ans = input()
+        if ans=="1":
+            print("Give username: ")
+            un = input()
+            print("Give user group (1 = admin, 2 = user): ")
+            ur = input()            
+            password = manager.create_password()
+            print("This is the password for user " + un + " : " + password)
+            user = User(un, password, ur)
+            manager.addUser(user)
+        elif ans=="2":
+            
+            if manager.deleteUser():
+                print("toimii")
                 
-        return hash_password(pwd)
+        elif ans=="3":
+            ans = None
+        else:
+            print("Not Valid Choice Try again")
 
 if __name__ == '__main__':
     main()
--- app/users/user_manager.py
+++ app/users/user_manager.py
@@ -1,14 +1,93 @@
+import hashlib
+import os, sys, uuid, random, string, pickle, csv
+from users.user import User, Role
+
 class UserManager:
     def __init__(self):
         self.user_list = []
 
 
     def addUser(self,user):
+        
+        user.password = self.hash_password(user.password)
+        user.create_keys()
         self.user_list.append(user)
+        tosave = []
+        for i in range(0, len(self.user_list)):
+            data = [self.user_list[i].username,
+                    self.user_list[i].password,
+                    self.user_list[i].role,]
+            
+            tosave.append(data)
+        
+        pwdFile = open(os.path.join(sys.path[0], "pwd.txt"), "a", newline="")
+        with pwdFile:
+            writer = csv.writer(pwdFile)
+            writer.writerows(tosave)
+           
+        print("\nSaved user....\n")
 
-    def find_user_with_id(self,toFind):
-        for user in self.user_list:
-            if user.username == toFind:
-                return user
+             
 
-        return None
+    def find_user_with_id(self,toFind, pwd):
+        with open(os.path.join(sys.path[0], "pwd.txt"), 'r') as pwdFile:        
+            reader = csv.reader(pwdFile)
+            for row in reader:
+                if row[0] == toFind and self.check_password(row[1], pwd):
+                    return int(row[2])
+                else:
+                    print("User not found")
+                    return None
+
+    def checkUser(self, toFind):
+        
+        with open(os.path.join(sys.path[0], "pwd.txt"), 'r', newline="") as pwdFile:
+            reader = csv.reader(pwdFile)
+            try:
+                for row in reader:
+                    if row[0] == toFind:
+                        return True
+            except IndexError:
+                pass
+
+    def deleteUser(self):
+        print("Give username to delete: ")
+        toFind = input()
+        user_check = self.checkUser(toFind)
+        if user_check:
+            with open(os.path.join(sys.path[0], "pwd.txt"), 'r', newline="") as pwdFile:        
+                data = list(csv.reader(pwdFile))
+            
+            with open(os.path.join(sys.path[0], "pwd.txt"), 'w', newline="") as pwdFile:
+                writer = csv.writer(pwdFile)
+                for row in data:
+                    if row[0] != toFind:
+                        writer.writerow(row)
+            print("User deleted")
+        else:
+            print("No user found")
+        
+                            
+
+
+    def create_password(self):
+        length = 10
+        lettersDigits = string.ascii_letters + string.digits
+        return ''.join(random.choice(lettersDigits) for i in range (length))
+
+
+    def hash_password(self, password):
+        # uuid is used to generate a random number of the password
+        salt = uuid.uuid4().hex
+        return hashlib.sha256(salt.encode() + password.encode()).hexdigest() + ":" + salt
+
+    def check_password(self, hashed_password, user_password):
+        password, salt = hashed_password.split(':')
+        return password == hashlib.sha256(salt.encode() + user_password.encode()).hexdigest()
+        
+       
+
+           
+
+
+    
